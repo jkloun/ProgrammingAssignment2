92be1e41141ee41273189b0739d486259b6adf8c
jkloun <34279611+jkloun@users.noreply.github.com>
2018-03-22 10:42:06 -0700

    Add files via upload
    jkloun's committed cachematrix.R submission for Programming Assignment 2: Lexical Scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+##my function will calculate the inverse of a Martix using the solve() function
+##it will then cache this inverse and be able to recall it in the case the matrix has not changed
+##thus reducing the number of times the matrix solution is calculated
 
-## Write a short comment describing this function
+
+
+##the following function uses the <<- operator to assign a value to an object in the cache environment
+##which is different from the current environment
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x<<- y
+    m<<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m<<-solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## The following function will calculate the inverse of the special matrix from the above function
+##HOWEVER, it will first check to see if the inverse has already been calculated and cached
+##if it has already been calculated, it will just return the cached solution
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+      message("getting cached data")
+      return(m)
+  }
+  data <- x$get()
+  m <- solve(data,...)
+  x$setsolve(m)
+  m
 }
